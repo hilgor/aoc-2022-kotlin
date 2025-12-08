2f2f591dcea7b74067f727266842a21523a8a9b1
Igor Ionov <igor.ionov.ext@uniper.energy>
2025-12-08 06:24:52 +0100

    Day 7

---
+++ src/main/kotlin/de/iits/aoc/_2025/Day07.kt
@@ -0,0 +1,56 @@
+package de.iits.aoc._2025
+
+import filledLines
+
+class Day07 {
+
+    fun calculate(inputStr: String) = inputStr.filledLines()
+        .let { field ->
+            val beams = mutableSetOf(field.first().indexOf('S'))
+
+            field.drop(1)
+                .fold(0) { splitCount, row ->
+                    splitCount + splitOnNextRow(row, beams)
+                }
+        }
+
+    private fun splitOnNextRow(row: String, beams: MutableSet<Int>): Int =
+        beams.fold(0 to mutableSetOf<Int>()) { (splits, newBeams), beam ->
+            if (row[beam] == '^') {
+                newBeams.add(beam - 1)
+                newBeams.add(beam + 1)
+                splits + 1 to newBeams
+            } else {
+                newBeams.add(beam)
+                splits to newBeams
+            }
+        }.let { (splits, newBeams) ->
+            beams.clear()
+            beams.addAll(newBeams)
+            splits
+        }
+
+
+    fun calculate2(inputStr: String) = inputStr.filledLines()
+        .let { field ->
+            processPath(field, 1, field.first().indexOf('S'), mutableMapOf())
+        }
+
+    private fun processPath(field: List<String>, row: Int, position: Int, cache: MutableMap<Pair<Int,Int>, Long>): Long =
+        when {
+            cache.containsKey(row to position) -> cache[row to position]!!
+            row >= field.size -> 1L
+            field[row][position] == '^' -> {
+                val result = processPath(field, row + 1, position - 1, cache) + processPath(field, row + 1, position + 1, cache)
+                cache[row to position] =  result
+                result
+            }
+
+
+            else -> {
+                processPath(field, row + 1, position, cache)
+            }
+        }
+
+}
+
---
+++ src/test/kotlin/de/iits/aoc/_2025/Day07Test.kt
@@ -0,0 +1,42 @@
+package de.iits.aoc._2025
+
+import getInputForDay
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class Day07Test {
+
+    private val sut = Day07()
+    private val day = 7
+
+    @Test
+    fun testExample() {
+        val result = sut.calculate(getInputForDay(day, example = true, 2025))
+        assertEquals(21, result)
+    }
+
+    @Test
+    fun testExample2() {
+        val result = sut.calculate2(getInputForDay(day, example = true, 2025))
+
+        assertEquals(40, result)
+    }
+
+
+    ///
+    ///
+
+
+
+    @Test
+    fun testInput() {
+        val result = sut.calculate(getInputForDay(day, false, 2025))
+        println(result) //1541
+    }
+
+    @Test
+    fun testInput2() {
+        val result = sut.calculate2(getInputForDay(day, false, 2025))
+        println(result)
+    } //80158285728929
+}
